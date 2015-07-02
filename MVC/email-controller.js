7d7ac5b0557572248db68fd84add5b68ca13d29c
Ken Chan <KChan923@gmail.com>
2015-07-01 23:44:10 -0400

    second revision on mvc

--- MVC/email-controller.js
+++ MVC/email-controller.js
@@ -1,49 +1,49 @@
 function EmailController(model, view) {
   // Store the provided Model and View objects
-  this.model = model;
-  this.view = view;
+  this._model = model;
+  this._view = view;
 }
 
 EmailController.prototype = {
-// Define a method to use to initialize the system, which gets the data from the Model using
-// its getAll() method and passes it to the associated View by executing that View's
-// render() method
-initialize: function() {
-// Get the list of email addresses from the associated Model
-var modelData = this.model.getAll();
-// Pass that data to the render() method of the associated View
-this.view.render(modelData);
-// Connect Controller logic to system-wide events
-this.bindEvents();
-},
-// Define a method for connecting Controller logic to system-wide events
-bindEvents: function() {
-var that = this;
-// When the View indicates that a new email address has been added via the user
-// interface, call the addEmail() method
-observer.subscribe("view.email-view.add", function(email) {
-that.addEmail(email);
-});
-// When the View indicates that an email address has been remove via the user
-// interface, call the removeEmail() method
-observer.subscribe("view.email-view.remove", function(email) {
-that.removeEmail(email);
-});
-},
-// Define a method for adding an email address to the Model, called when an email address
-// has been added via the View's user interface
-addEmail: function(email) {
-// Call the add() method on the Model directly, passing the email address added via
-// the View. The Model will then broadcast an event indicating a new email address has
-// been added, and the View will respond to this event directly, updating the UI
-this.model.add(email);
-},
-// Define a method for removing an email address from the Model, called when an email
-// address has been removed via the View's user interface
-removeEmail: function(email) {
-// Call the remove() method on the Model directly, passing the email address added via
-// the View. The Model will then broadcast an event indicating an email address has
-// been removed, and the View will respond to this event directly, updating the UI
-this.model.remove(email);
-}
+  // Define a method to use to initialize the system, which gets the data from the Model using
+  // its getAll() method and passes it to the associated View by executing that View's
+  // render() method
+  initialize: function() {
+    // Get the list of email addresses from the associated Model
+    var modelData = this._model.getAll();
+    // Pass that data to the render() method of the associated View
+    this._view.render(modelData);
+    // Connect Controller logic to system-wide events
+    this.bindEvents();
+  },
+  // Define a method for connecting Controller logic to system-wide events
+  bindEvents: function() {
+    var that = this;
+    // When the View indicates that a new email address has been added via the user
+    // interface, call the addEmail() method
+    observer.subscribe("view.email-view.add", function(email) {
+      that.addEmail(email);
+    });
+    // When the View indicates that an email address has been remove via the user
+    // interface, call the removeEmail() method
+    observer.subscribe("view.email-view.remove", function(email) {
+      that.removeEmail(email);
+    });
+  },
+  // Define a method for adding an email address to the Model, called when an email address
+  // has been added via the View's user interface
+  addEmail: function(email) {
+    // Call the add() method on the Model directly, passing the email address added via
+    // the View. The Model will then broadcast an event indicating a new email address has
+    // been added, and the View will respond to this event directly, updating the UI
+    this._model.add(email);
+  },
+  // Define a method for removing an email address from the Model, called when an email
+  // address has been removed via the View's user interface
+  removeEmail: function(email) {
+    // Call the remove() method on the Model directly, passing the email address added via
+    // the View. The Model will then broadcast an event indicating an email address has
+    // been removed, and the View will respond to this event directly, updating the UI
+    this._model.remove(email);
+  }
 };
--- MVC/email-demo.js
+++ MVC/email-demo.js
@@ -1,22 +1,21 @@
-// Create an instance of our email Model "class", populating it with a few email addresses to
-// get started with
+//  Populate the model with example e-mail addresses.
 var emailModel = new EmailModel([
   "foo@gmail.com",
   "bar@gmail.com",
   "baz@gmail.com",
   "qux@gmail.com"
-]),
+]);
 
 // Create instances of our form View and list View "classes"
-emailFormView = new EmailFormView(),
-emailListView = new EmailListView(),
+var emailFormView = new EmailFormView();
+var emailListView = new EmailListView();
 // Combine together the form and list Views as children of a single View object
-emailView = new EmailView([emailFormView, emailListView]),
+var emailView = new EmailView([emailFormView, emailListView]);
 // Create an instance of our email system Controller, passing it the Model instance and
 // the View to use. Note that the Controller does not need to be aware whether the View
 // contains a single View or multiple, combined Views, as it does here - this is an example
 // of the composite pattern in action
-emailController = new EmailController(emailModel, emailView);
+var emailController = new EmailController(emailModel, emailView);
 // Finally, initialize the Controller which gets the data from the Model and passes it to the
 // render() method of the View, which, in turn, connects up the user interface to the
 // system-wide events, bringing the whole application together
