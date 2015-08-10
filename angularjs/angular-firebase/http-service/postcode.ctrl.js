171b78a09933748381fcaa97ee254ddc355974a3
Ken Chan <KChan923@gmail.com>
2015-08-10 16:02:48 -0400

    more notes on  and  services

---
+++ angularjs/angular-firebase/http-service/postcode.ctrl.js
@@ -0,0 +1,10 @@
+'use strict';
+
+app.controller('PostCodeController', ['$scope', 'postCodeService', function($scope, postCodeService){
+    $scope.postCodeData = {};
+
+    var success = function(data, status, headers, config){
+      
+    }
+  }
+])
