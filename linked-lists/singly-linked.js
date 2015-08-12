b4f11215cf3376546a74bb363098628b6efb806f
Ken Chan <KChan923@gmail.com>
2015-08-12 00:17:49 -0400

    added several new functions for linked list

--- linked-lists/singly-linked.js
+++ linked-lists/singly-linked.js
@@ -1,36 +1,127 @@
+var Node = require('./node');
+
 /**
- * A node is an element of a list.
- * It is comprised of two items - the data and a reference to the next node.
- * @param {int}
- * @return {Node}
+ * A linked list is a linear data structure where each element is a separate object.
+ * Given its a dynamic data structure, the number of nodes is not fixed, and the list
+ * can grow and shrink on demand.
+ * @param {void}
+ * @return {LinkedList}
  */
 var LinkedList = module.exports = function LinkedList(){
-  this.head = new Node()
+  this.head = null;
+  this.length = 0;
 };
 
 /**
- * Getter method for obtaining data of a node.
- * @param {void}
- * @return {int}
- */
-Node.prototype.getData = function(){
-  return this.data;
-};
-
-/**
- * Getter method for obtaining subsequent node of a node.
- * @param {void}
- * @return {Node}
- */
-Node.prototype.getNext = function(){
-  return this.next;
-};
-
-/**
- * Setter method for settting subsequent node for a node.
- * @param {Node}
+ * Add a new item to the end of the list.
+ * @param {int}
  * @return {void}
  */
-Node.prototype.setNext = function(next){
-  this.next = next;
+LinkedList.prototype.append = function(data){
+  var current;
+  var node = new Node(data);
+
+  //  If the linked list has no element, make the head the new node.
+  if(!head){
+    this.head = node;
+  }
+  else{
+    current = this.head;
+
+    while(current.next){
+      current = current.getNext();
+    }
+
+    //  Iterate until the last item is obtained.
+    //  The last item has no next node.
+    current.setNext(node);
+  }
+
+  this.length++;
+};
+
+/**
+ *
+ * @param {}
+ * @return {}
+ */
+LinkedList.prototype.insert = function(position, data){
+
+};
+
+/**
+ *
+ * @param {}
+ * @return {}
+ */
+LinkedList.prototype.remove = function(){
+
+};
+
+/**
+ *
+ * @param {}
+ * @return {}
+ */
+LinkedList.prototype.indexOf = function(){
+
+};
+
+/**
+ *
+ * @param {}
+ * @return {}
+ */
+LinkedList.prototype.removeAt = function(){
+
+};
+
+/**
+ *
+ * @param {}
+ * @return {}
+ */
+LinkedList.prototype.isEmpty = function(){
+  return this.length === 0;
+};
+
+/**
+ *
+ * @param {}
+ * @return {}
+ */
+LinkedList.prototype.size = function(){
+  return this.length;
+};
+
+/**
+ *
+ * @param {}
+ * @return {}
+ */
+LinkedList.prototype.getHead = function(){
+  return this.head;
+};
+
+/**
+ *
+ * @param {}
+ * @return {}
+ */
+LinkedList.prototype.toString = function(){
+  var string = '[';
+  var current = this.head;
+
+  while(current){
+    string += current.getData();
+    current = current.getNext();
+    if(current){
+      string += ' ';
+    }
+    else{
+      string += ']'
+    }
+  }
+
+  return string;
 };
