0233f0b8f3455aed63fb8b883a3b54f71f53a960
Ken Chan <KChan923@gmail.com>
2015-08-11 22:58:53 -0400

    added linked list script

--- linked-lists/singly-linked.js
+++ linked-lists/singly-linked.js
@@ -1,14 +1,36 @@
-//  Singly Linked List
-
 /**
- *
- */
-
-/**
+ * A node is an element of a list.
+ * It is comprised of two items - the data and a reference to the next node.
  * @param {int}
  * @return {Node}
- * @
  */
-var Node = module.exports = function Node(data){
-  
+var LinkedList = module.exports = function LinkedList(){
+  this.head = new Node()
+};
+
+/**
+ * Getter method for obtaining data of a node.
+ * @param {void}
+ * @return {int}
+ */
+Node.prototype.getData = function(){
+  return this.data;
+};
+
+/**
+ * Getter method for obtaining subsequent node of a node.
+ * @param {void}
+ * @return {Node}
+ */
+Node.prototype.getNext = function(){
+  return this.next;
+};
+
+/**
+ * Setter method for settting subsequent node for a node.
+ * @param {Node}
+ * @return {void}
+ */
+Node.prototype.setNext = function(next){
+  this.next = next;
 };
