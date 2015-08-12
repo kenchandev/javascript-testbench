7330a4b9635e112eab855d02d78ce44828093f92
Ken Chan <KChan923@gmail.com>
2015-08-11 22:52:26 -0400

    added node script

---
+++ linked-lists/node.js
@@ -0,0 +1,37 @@
+/**
+ * A node is an element of a list.
+ * It is comprised of two items - the data and a reference to the next node.
+ * @param {int}
+ * @return {Node}
+ */
+var Node = module.exports = function Node(data){
+  this.data = data;
+  this.next = null;
+};
+
+/**
+ * Getter method for obtaining data of a node.
+ * @param {void}
+ * @return {int}
+ */
+Node.prototype.getData = function(){
+  return this.data;
+};
+
+/**
+ * Getter method for obtaining subsequent node of a node.
+ * @param {void}
+ * @return {Node}
+ */
+Node.prototype.getNext = function(){
+  return this.next;
+};
+
+/**
+ * Setter method for settting subsequent node for a node.
+ * @param {Node}
+ * @return {void}
+ */
+Node.prototype.setNext = function(next){
+  this.next = next;
+};
---
+++ linked-lists/singly-linked.js
@@ -0,0 +1,14 @@
+//  Singly Linked List
+
+/**
+ *
+ */
+
+/**
+ * @param {int}
+ * @return {Node}
+ * @
+ */
+var Node = module.exports = function Node(data){
+  
+};
