33d716bee7d0b8ed1b7abca32ef155ec02f42149
Ken Chan <KChan923@gmail.com>
2015-08-12 03:42:04 -0400

    added .gitignore

---
+++ linked-lists/index.js
@@ -0,0 +1,13 @@
+/**
+ * Written by Ken Chan
+ * MIT Licensed
+ */
+
+'use strict';
+
+module.exports = {
+  node: require('./lib/node'),
+  singly: require('./lib/singly-linked'),
+  doubly: require('./lib/doubly-linked'),
+  circularly: require('./lib/circularly-linked')
+};
