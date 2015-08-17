667928f8ca35435edd93fa974efad9c4636deed0
Ken Chan <KChan923@gmail.com>
2015-08-17 14:15:20 -0400

    finished insertion sort

--- algorithms/sort/lib/bubble_sort.js
+++ algorithms/sort/lib/bubble_sort.js
@@ -4,7 +4,7 @@ var swap = require('./swap');
 
 (function(){
   /**
-   * Bubble Sort: O(n^2) algorithm for naively sorting an array of elements
+   * Bubble Sort: O(n^2) algorithm for naively sorting an array of elements in-place.
    *              The array is sorted in ascending order by performing multiple swaps.
    *              Finds the largest element in a given subset, and gradually swaps it towards the right.
    * 45 67 12 34 25 39
--- algorithms/sort/lib/insertion_sort.js
+++ algorithms/sort/lib/insertion_sort.js
@@ -1,27 +1,37 @@
 'use strict';
 
-var swap = require('./swap');
-
 (function(){
   /**
-   * Insertion Sort: O(n^2) algorithm for naively sorting an array of elements
-   *                 The array is sorted in ascending order by performing multiple swaps.
-   *                 Finds the largest element in a given subset, and gradually swaps it towards the right.
+   * Insertion Sort: O(n^2) algorithm for sorting an array of elements in-place
    *                 (Has better overall performance compared to selection and bubble sorts)
-   * 45 67 12 34 25 39
+   * 42 16 84 12 77 26 53
    *
    * Example iteration:
-   * 45 67 12 34 25 39    (Compared 45 and 67, left them be)
-   * 45 12 67 34 25 39    (Compared 67 and 12, swapped them)
-   * 45 12 34 67 25 39    (Compared 67 and 34, swapped them)
-   * 45 12 34 25 67 39    (Compared 67 and 25, swapped them)
-   * 45 12 34 25 39 67    (Compared 67 and 39, swapped them)
+   * 42 16 84 12 77 26 53    (Started off with 42 in the left subsection)
+   * 16 42 84 12 77 26 53    (Introduced 16 to the left subsection, and shifted 42 to make room)
+   * 16 42 84 12 77 26 53    (Introduced 84 to the left subsection, and left as is)
+   * 12 16 42 84 77 26 53    (Introduced 12 to the left subsection, and shifted 16, 42 and 84 to make room)
+   * (At this point, the remaining work can be derived easily)
    *
    * @param {Array}
    * @return {void}
    */
   var insertionSort = function(arr){
+    var length = arr.length;
+    var j, temp;
 
+    //  First element is already in the subsection by default. Hence, start at position 1.
+    for(var i = 1; i < length; i++){
+      j = i;
+      temp = arr[i];
+
+      while(j > 0 && arr[j - 1] > temp){
+        arr[j] = arr[j-1];
+        j--;
+      }
+
+      arr[j] = temp;
+    }
   };
 
   module.exports = insertionSort;
