ff3a9843490bad2dc3f68dc54bddf925295f00bf
Ken Chan <KChan923@gmail.com>
2015-08-18 01:33:16 -0400

    added print function

--- dictionary/lib/hash-table.js
+++ dictionary/lib/hash-table.js
@@ -11,6 +11,21 @@
   };
 
   /**
+   * Auxiliary hash function for hash table to minimize hash collisions.
+   * @param {*}
+   * @return {int}
+   */
+  var djb2HashCode = function(key){
+    var hash = 5381;
+
+    for(var i = 0; i < key.length; i++){
+      hash = hash * 33 + key.charCodeAt(i); //  charCodeAt returns unicode value for character at position i.
+    }
+
+    return hash % 1013;
+  };
+
+  /**
    * Adds a new item to the hash table.
    * @param {*}
    * @param {*}
@@ -34,10 +49,23 @@
   /**
    * Removes a value from the hash table.
    * @param {*}
-   * @return {*}
+   * @return {void}
    */
-  HashTable.prototype.get = function(){
+  HashTable.prototype.remove = function(key){
     table[(key)] = undefined;
+  };
+
+  /**
+   * Prints hash table.
+   * @param {void}
+   * @return {void}
+   */
+  HashTable.prototype.print = function(){
+    for(var i = 0; i < this.table.length; i++){
+      if(this.table[i] !== undefined){
+        console.log(i + ': ' + this.table[i]);
+      }
+    }
   };
 
   module.exports = HashTable;
